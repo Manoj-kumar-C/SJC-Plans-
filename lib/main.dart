53bca49e94755f3b990b38245ec3adb0e5dacb56
manoj-chandran <developermanoj2003@gmail.com>
2023-09-02 11:36:38 +0530

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,20 +21,27 @@ class HomeScreen extends StatelessWidget {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Popup Dialog Title'),
+          title: Text('Announcement'),
           content: SingleChildScrollView(
             child: ListBody(
               children: <Widget>[
-                Text('This is a sample popup dialog message.'),
-                Text('You can customize it as per your needs.'),
+                Text('New Scheme Started on December'),
+                Text('2021'),
               ],
             ),
           ),
           actions: <Widget>[
             TextButton(
-              child: Text('Close'),
+              child: Text('Yes'),
               onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
+                // Close the dialog
+                Navigator.of(context).pop();
+
+                // Navigate to the ImageSliderPage
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ImageSliderPage()),
+                );
               },
             ),
           ],
@@ -44,31 +54,188 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Popup Dialog Example'),
+        flexibleSpace: Image(
+          // Replace with your image path
+          image: AssetImage('../images/logo.jpg'),
+          fit: BoxFit.cover,
+        ),
+        title: Text(''),
       ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(50.0),
-          child: Column(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(50.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Flexible(
+                  // Make the image responsive
+                  child: GestureDetector(
+                    onTap: () {
+                      _showPopupDialog(context);
+                    },
+                    child: Image.asset(
+                        '../images/image1.jpg'), // Replace with your image path
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(50.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Flexible(
+                  // Make the image responsive
+                  child: GestureDetector(
+                    onTap: () {
+                      _showPopupDialog(context);
+                    },
+                    child: Image.asset(
+                        '../images/image2.jpg'), // Replace with your image path
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                InkWell(
+                  onTap: () async {
+                    const facebookUrl =
+                        'https://www.facebook.com/your-facebook-profile-url';
+                    if (await canLaunch(facebookUrl)) {
+                      await launch(facebookUrl);
+                    } else {
+                      throw 'Could not launch $facebookUrl';
+                    }
+                  },
+                  child: Icon(FontAwesomeIcons.facebook),
+                ),
+                InkWell(
+                  onTap: () async {
+                    const twitterUrl =
+                        'https://twitter.com/your-twitter-profile-url';
+                    if (await canLaunch(twitterUrl)) {
+                      await launch(twitterUrl);
+                    } else {
+                      throw 'Could not launch $twitterUrl';
+                    }
+                  },
+                  child: Icon(FontAwesomeIcons.twitter),
+                ),
+                InkWell(
+                  onTap: () async {
+                    const instagramUrl =
+                        'https://www.instagram.com/fresh_spar/';
+                    if (await canLaunch(instagramUrl)) {
+                      await launch(instagramUrl);
+                    } else {
+                      throw 'Could not launch $instagramUrl';
+                    }
+                  },
+                  child: Icon(FontAwesomeIcons.instagram),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ImageSliderPage extends StatefulWidget {
+  const ImageSliderPage({Key? key}) : super(key: key);
+
+  @override
+  State<ImageSliderPage> createState() => _ImageSliderPageState();
+}
+
+class _ImageSliderPageState extends State<ImageSliderPage> {
+  List imageList = [
+    {"id": 1, "image_path": '../images/one.jpg'},
+    {"id": 2, "image_path": '../images/two.jpg'},
+    {"id": 3, "image_path": '../images/three.jpg'},
+    {"id": 4, "image_path": '../images/four.jpg'},
+    {"id": 5, "image_path": '../images/five.jpg'},
+    {"id": 6, "image_path": '../images/six.jpg'},
+    {"id": 7, "image_path": '../images/seven.jpg'},
+    {"id": 8, "image_path": '../images/eight.jpg'},
+  ];
+  final CarouselController carouselController = CarouselController();
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null, // Set the AppBar to null to remove it
+      body: Column(
+        children: [
+          Stack(
             children: [
-              GestureDetector(
+              InkWell(
                 onTap: () {
-                  _showPopupDialog(context);
+                  print(currentIndex);
                 },
-                child: Image.asset(
-                    '../images/image1.jpg'), // Replace with your image path
+                child: CarouselSlider(
+                  items: imageList
+                      .map(
+                        (item) => Image.asset(
+                          item['image_path'],
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                      )
+                      .toList(),
+                  carouselController: carouselController,
+                  options: CarouselOptions(
+                    scrollPhysics: const BouncingScrollPhysics(),
+                    autoPlay: true,
+                    aspectRatio: 2,
+                    viewportFraction: 1,
+                    height: 800,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        currentIndex = index;
+                      });
+                    },
+                  ),
+                ),
               ),
-              SizedBox(height: 50.0),
-              GestureDetector(
-                onTap: () {
-                  _showPopupDialog(context);
-                },
-                child: Image.asset(
-                    '../images/image2.jpg'), // Replace with your image path
+              Positioned(
+                bottom: 10,
+                left: 0,
+                right: 0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: imageList.asMap().entries.map((entry) {
+                    return GestureDetector(
+                      onTap: () => carouselController.animateToPage(entry.key),
+                      child: Container(
+                        width: currentIndex == entry.key ? 17 : 7,
+                        height: 7.0,
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 3.0,
+                        ),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: currentIndex == entry.key
+                                ? Colors.red
+                                : Colors.teal),
+                      ),
+                    );
+                  }).toList(),
+                ),
               ),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
